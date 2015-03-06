2ae1dbb1a2b42341e84908622ebfe15827f38f5c
Stud <vct.studer@gmail.com>
2015-03-06 07:23:27 +0100

    Ajout de purse et income

---
+++ Assets/Game/Money/Income.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Income : MonoBehaviour {
+
+    private int currentIncome;
+    [SerializeField]
+    private int startIncome;
+    [SerializeField]
+    private float incomeCooldown;
+
+    private Purse purse;
+
+	// Use this for initialization
+	void Start () {
+        purse = GetComponent<Purse>();
+        currentIncome = startIncome;
+
+        StartCoroutine(incomeCoroutine());
+	}
+	
+    IEnumerator incomeCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(incomeCooldown);
+            purse.add(currentIncome);
+        }
+    }
+
+    public void increaseIncome(int amount)
+    {
+        currentIncome += amount;
+    }
+}
---
+++ Assets/Game/Money/Purse.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Purse : MonoBehaviour {
+
+    private int currentAmount;
+
+    [SerializeField]
+    private int startAmount;
+
+    void Start()
+    {
+        currentAmount = startAmount;
+    }
+
+    public void add(int amount)
+    {
+        currentAmount += amount;
+    }
+
+    public bool canAfford(int amount)
+    {
+        return amount < currentAmount;
+    }
+
+    public void substract(int amount)
+    {
+        if (canAfford(amount))
+            currentAmount -= amount;
+    }
+}
