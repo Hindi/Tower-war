a4c0132e67da6094babebca170e6bbd2bcad3c5b
Stud <vct.studer@gmail.com>
2015-03-15 16:19:15 +0100

    UILife

---
+++ Assets/Game/UI/UILife.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UILife : MonoBehaviour 
+{
+    [SerializeField]
+    private Text myLifeLabel;
+    [SerializeField]
+    private Text enemyLifeLabel;
+
+    public void updateMyLifeLabel(int value)
+    {
+        myLifeLabel.text = value.ToString();
+    }
+
+    public void updateEnemyLifeLabel(int value)
+    {
+        enemyLifeLabel.text = value.ToString();
+    }
+}
